a0267ad5845caa4646ab61974c97135eb782967f
weichung034918 <weichung034918@gmail.com>
2016-03-21 13:34:55 +0800

    new file i tri evertiem

--- src/reservation/Reservation_control.java
+++ src/reservation/Reservation_control.java
@@ -11,20 +11,27 @@ import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.io.IOException;
+
 
 public class Reservation_control {
     ObservableList<String> gtitle = FXCollections.observableArrayList("Mr.", "Mrs.");
+    ObservableList<String> roomtype = FXCollections.observableArrayList("Commercial","Suite","President");
+
     @FXML private ComboBox titlecombo;
-    @FXML private ComboBox idcombo;
+    @FXML private ComboBox sidcombo;
     @FXML private ComboBox nextbtn;
+    @FXML private ComboBox roomtypecombo;
+
     @FXML
     private void initialize() {
     titlecombo.setItems(gtitle);
-
+    roomtypecombo.setItems(roomtype);
     }
-    @FXML private void nextbtn(ActionEvent next){
-       Parent root = FXMLLoader.load(getClass().getResource(""));
+
+    @FXML private void nextbtn(ActionEvent next) throws IOException {
+       Parent root = FXMLLoader.load(getClass().getResource("R_roominfo.fxml"));
         Scene scene = new Scene(root);
-       .setScene(scene);
+
     }
 }
