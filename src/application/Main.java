e53cc6690159c4d876f2e39eb68061fb723319d4
lolzcalvin <brotherken1996@gmail.com>
2016-03-28 21:58:48 +0900

    Moved DB (28/03) Added initialisation of building artifacts (deployment) Handled connection to db

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,22 +3,46 @@ package application;
 import application.assets.Login;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Main extends Application {
     @Override
-    public void start(Stage P) throws Exception{
+    public void start(Stage P) throws Exception {
+        if (!connStatus()){ //if connStatus() == false
+            //do sth, haven't got the idea to do what first, but should be doing sql query to create db
+        }
         P.setTitle("Hotel Management System");
         P.setScene(this.newScene());
         P.show();
     }
 
-    private Scene newScene(){
+    public Boolean connStatus(){
+        Connection c = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:Data.sqlite");
+            return true;
+        } catch (Exception e) {
+            Alert conAlert = new Alert(Alert.AlertType.ERROR);
+            conAlert.setTitle("Error");
+            conAlert.setHeaderText("Database Connection Problem");
+            conAlert.setContentText(e.getClass().getName() + ": " + e.getMessage());
+            conAlert.showAndWait();
+        }
+        return false;
+    }
+
+    private Scene newScene() {
         return new Scene(new Login().loginStage());
     }
 
     public static void main(String[] args) {
         launch(args);
-    }
+    }//please don't care this main method
 }
 
