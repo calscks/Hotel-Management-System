110e9de08efdea2b3cf7d633645a3faba4a61150
lolzcalvin <brotherken1996@gmail.com>
2016-03-30 18:59:58 +0900

    Tried implementing a side menu But I havent tried it yet

--- src/application/slidemenu/SlideMenuController.java
+++ src/application/slidemenu/SlideMenuController.java
@@ -1,4 +1,40 @@
 package application.slidemenu;
 
-public class SlideMenuController {
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import javafx.event.ActionEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SlideMenuController implements Initializable {
+
+    @FXML
+    private Button btn_Menu;
+    @FXML
+    private AnchorPane leftMenu;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        SlideMenuAnimate();
+    }
+
+    private void SlideMenuAnimate(){
+        TranslateTransition openMenu = new TranslateTransition(new Duration(350), leftMenu);
+        openMenu.setToX(0);
+        TranslateTransition closeMenu = new TranslateTransition(new Duration(350), leftMenu);
+        btn_Menu.setOnAction((ActionEvent evt)->{
+            if (leftMenu.getTranslateX()!=0) {
+                openMenu.play();
+            } else {
+                closeMenu.setToX(-(leftMenu.getWidth()));
+                closeMenu.play();
+            }
+        });
+    }
+
+
 }
