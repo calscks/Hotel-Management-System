2732a64d8b8d513c678f7c891e530ab94067681b
weichung034918 <weichung034918@gmail.com>
2016-04-03 10:41:19 +0800

    abort services and changed to bed type

--- src/application/ABORTEDservices/ModServController.java
+++ src/application/ABORTEDservices/ModServController.java
@@ -1,11 +1,11 @@
-package application.services;/**
+package application.ABORTEDservices;/**
  * Created by User on 2/4/2016.
  */
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class AddServController extends Application {
+public class ModServController extends Application {
 
     public static void main(String[] args) {
         launch(args);
