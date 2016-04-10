f0d6f79337faa515686969be5ed8d32bd973567c
lolzcalvin <brotherken1996@gmail.com>
2016-04-11 01:40:22 +0900

    open a window for add guest

--- src/application/assets/reservation/ResvController.java
+++ src/application/assets/reservation/ResvController.java
@@ -10,9 +10,14 @@ import javafx.fxml.FXML;
 import application.Validation;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -20,20 +25,7 @@ import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
 public class ResvController implements Initializable{
-    /*private int year1 = Calendar.getInstance().get(Calendar.YEAR);
-    private int year2 = Calendar.getInstance().get(Calendar.YEAR) + 1;
-    private int year3 = Calendar.getInstance().get(Calendar.YEAR) + 2;
-    private int year4 = Calendar.getInstance().get(Calendar.YEAR) + 3;
-    private int year5 = Calendar.getInstance().get(Calendar.YEAR) + 4;
-    private int year6 = Calendar.getInstance().get(Calendar.YEAR) + 5;
-    private int year7 = Calendar.getInstance().get(Calendar.YEAR) + 6;
 
-    private ObservableList<String> gtitle = FXCollections.observableArrayList("Mr.", "Mrs.");
-    private ObservableList<String> roomtype = FXCollections.observableArrayList("Commercial", "Suite", "President");
-    private ObservableList<String> paymenttype = FXCollections.observableArrayList("Credit Card", "Cash");
-    private ObservableList<Integer> month = FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
-    private ObservableList<Integer> year = FXCollections.observableArrayList(year1, year2, year3, year4, year5, year6, year7);
-*/
     @FXML private AnchorPane resvPane;
     @FXML private TextField tf_fname;
     @FXML private TextField tf_lname;
@@ -64,7 +56,10 @@ public class ResvController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
         validations();
+
+        addGuest();
 
         //store short form of countries in array
         String [] locale = Locale.getISOCountries();
@@ -107,6 +102,29 @@ public class ResvController implements Initializable{
             //when the timeline is finished (finished fade out) then invoke remove the finalPayment
             timeline.setOnFinished(se-> resvPane.getChildren().removeAll(finalPayment));
             timeline.play();
+        });
+    }
+
+    public void addGuest(){
+        btn_addguest.setOnMouseClicked(me->{
+            Stage guestStage = new Stage();
+            StackPane guestPane = new StackPane();
+            ScrollPane rootPane = new ScrollPane(guestPane);
+            FXMLLoader loadGuest = new FXMLLoader(getClass().getResource("/application/assets" +
+                    "/reservation/resvaddgroup.fxml"));
+            try {
+                Parent root = loadGuest.load();
+                guestPane.getChildren().addAll(root);
+                Scene guestScene = new Scene(rootPane);
+                guestStage.setScene(guestScene);
+                //line below blocks all inputs to the stage behind
+                guestStage.initModality(Modality.APPLICATION_MODAL);
+                guestStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            guestStage.setResizable(false); //disable resize
+            guestStage.setAlwaysOnTop(true); //set topmost
         });
     }
 
--- src/application/assets/reservation/ResvPayController.java
+++ src/application/assets/reservation/ResvPayController.java
@@ -1,10 +1,25 @@
 package application.assets.reservation;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
+import java.util.Calendar;
+
 public class ResvPayController{
+    private int year1 = Calendar.getInstance().get(Calendar.YEAR);
+    private int year2 = Calendar.getInstance().get(Calendar.YEAR) + 1;
+    private int year3 = Calendar.getInstance().get(Calendar.YEAR) + 2;
+    private int year4 = Calendar.getInstance().get(Calendar.YEAR) + 3;
+    private int year5 = Calendar.getInstance().get(Calendar.YEAR) + 4;
+    private int year6 = Calendar.getInstance().get(Calendar.YEAR) + 5;
+    private int year7 = Calendar.getInstance().get(Calendar.YEAR) + 6;
+    private ObservableList<Integer> month = FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+    private ObservableList<Integer> year = FXCollections.observableArrayList(year1, year2, year3, year4, year5, year6, year7);
+    private ObservableList<String> paymenttype = FXCollections.observableArrayList("Credit Card", "Cash");
+
     @FXML
     private Button btn_resvBack;
     @FXML
