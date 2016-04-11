eced7c446087a89e7179ca3a330299bb9fb087ab
lolzcalvin <brotherken1996@gmail.com>
2016-04-11 13:18:46 +0900

    some changes

--- src/application/assets/reservation/ResvAddGroupController.java
+++ src/application/assets/reservation/ResvAddGroupController.java
@@ -2,10 +2,20 @@ package application.assets.reservation;
 
 
 
+import application.DBConnection;
+import application.Validation;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ResvAddGroupController implements Initializable{
+public class ResvAddGroupController implements Initializable {
     @FXML private TextField tf_fname;
     @FXML private TextField tf_lname;
     @FXML private TextField tf_idno;
