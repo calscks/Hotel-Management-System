73f2e6860040d38fee54baee83b1d55f5039f94f
Phang Lik Haeng <hotlinkdigi90@hotmail.com>
2016-04-11 21:15:49 +0800

    modroom fxid fix, modfac label change when cbox change done

--- src/application/assets/RnFManagement/ModFacController.java
+++ src/application/assets/RnFManagement/ModFacController.java
@@ -1,10 +1,16 @@
 package application.assets.RnFManagement;
 
 import application.Validation;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
+
+import javax.activation.CommandObject;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,16 +20,36 @@ import java.util.ResourceBundle;
 
 public class ModFacController implements Initializable{
 
+    ObservableList<String> selectbyitems = FXCollections.observableArrayList("FacilityID","FacilityName","FacilityStatus");
+
     @FXML private TextField tf_searchby;
     @FXML private TextField tf_facno;
     @FXML private TextField tf_facname;
     @FXML private TextField tf_morning;
     @FXML private TextField tf_night;
     @FXML private TextField tf_wholeday;
+    @FXML private Label lbl_cboxselection;
+    @FXML private ComboBox cbox_searchby;
+
+    @FXML
+    private void labelchange(){
+        if (cbox_searchby.getValue().equals("FacilityID")){
+            lbl_cboxselection.setText("FacilityID :");
+        }
+        else if (cbox_searchby.getValue().equals("FacilityName")){
+            lbl_cboxselection.setText("FacilityName :");
+        }
+        else{
+            lbl_cboxselection.setText("FacilityStatus :");
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         validation();
+
+        cbox_searchby.setItems(selectbyitems);
     }
 
     private void validation() {
