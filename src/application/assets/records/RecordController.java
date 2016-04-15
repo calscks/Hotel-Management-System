2dac0cccd5449038de9995556096eec7ef8ce390
weichung034918 <weichung034918@gmail.com>
2016-04-16 03:26:01 +0800

    starting search record going to sleep

--- src/application/assets/records/RecordController.java
+++ src/application/assets/records/RecordController.java
@@ -1,7 +1,18 @@
 package application.assets.records;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by User on 4/4/2016.
  */
-public class RecordController {
+public class RecordController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
 }
