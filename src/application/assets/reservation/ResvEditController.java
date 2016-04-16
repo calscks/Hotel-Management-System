1d10c261b90e497b2f6b79538a71dcecd593e325
lolzcalvin <brotherken1996@gmail.com>
2016-04-16 14:57:58 +0900

    chnages on db nuked resvedit because not appropriate for current flow

--- src/application/assets/reservation/ResvEditController.java
+++ src/application/assets/reservation/ResvEditController.java
@@ -5,235 +5,87 @@ import application.assets.ModelFacility;
 import application.assets.ModelGroupMember;
 import application.assets.ModelRoom;
 import application.assets.ModelResv;
-import application.slidemenu.SlideMenuController;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import static application.slidemenu.SlideMenuController.db;
 
 public class ResvEditController implements Initializable {
 
-    @FXML
-    private Button btn_ciNext;
-    @FXML
-    private TextField tf_search;
-    @FXML
-    private Button btn_search;
-    @FXML
-    private TextField tf_resvno;
-    @FXML
-    private TextField tf_fname;
-    @FXML
-    private TextField tf_lname;
-    @FXML
-    private ComboBox resvIdType_CBox;
-    @FXML
-    private TextField tf_address;
-    @FXML
-    private TextField tf_postcode;
-    @FXML
-    private TextField tf_city;
-    @FXML
-    private TextField tf_idtype;
-    @FXML
-    private TextField tf_idno;
-    @FXML
-    private Button btn_ciAddGroupMember;
-    @FXML
-    private Button btn_ciAddRoom;
-    @FXML
-    private TableView<ModelGroupMember> guestTable;
-    @FXML
-    private TableColumn<ModelGroupMember, String> fname;
-    @FXML
-    private TableColumn<ModelGroupMember, String> lname;
-    @FXML
-    private TableColumn<ModelGroupMember, String> passporticno;
-    @FXML
-    private TableColumn<ModelGroupMember, String> roomno;
-    @FXML
-    private TableView<ModelRoom> roomTable;
-    @FXML
-    private TableColumn<ModelRoom, String> roomCat;
-    @FXML
-    private TableColumn<ModelRoom, String> roomType;
-    @FXML
-    private TableColumn<ModelRoom, String> bookDate;
-    @FXML
-    private TableColumn<ModelRoom, String> endDate;
-    @FXML
-    private TableView<ModelFacility> facilityTable;
-    @FXML
-    private TableColumn<ModelFacility, String> bookfac;
-    @FXML
-    private TableColumn<ModelFacility, String> facilityprice;
-    @FXML
-    private TableColumn<ModelFacility, String> bookdate;
-    @FXML
-    private TableColumn<ModelFacility, String> booktime;
-    @FXML
-    private TableView<ModelResv> table_sresult;
-    @FXML
-    private TableColumn<ModelResv, String> tbcol_sresv;
-    @FXML
-    private TableColumn<ModelResv, String> tbcol_sfname;
-    @FXML
-    private TableColumn<ModelResv, String> tbcol_slname;
-    @FXML
-    private TableColumn<ModelResv, String> tbcol_sroomno;
+    @FXML private TextField tf_search;
+    @FXML private TextField tf_resvno;
+    @FXML private TextField tf_fname;
+    @FXML private TextField tf_lname;
+    @FXML private TextField tf_address;
+    @FXML private TextField tf_postcode;
+    @FXML private TextField tf_city;
+    @FXML private TextField tf_state;
+    @FXML private TextField tf_idno;
+    @FXML private ComboBox cbox_country;
+    @FXML private ComboBox cbox_idtype;
+
+    @FXML private ToggleGroup ResvSearchGroup;
+    @FXML private RadioButton rb_resvno;
+    @FXML private RadioButton rb_roomno;
+    @FXML private RadioButton rb_lname;
+    @FXML private RadioButton rb_fname;
+
+    @FXML private Button btn_search;
+    @FXML private Button btn_resvcancel;
+    @FXML private Button btn_addfac;
+    @FXML private Button btn_delfac;
+    @FXML private Button btn_addroom;
+    @FXML private Button btn_delroom;
+    @FXML private Button btn_addguest;
+    @FXML private Button btn_delguest;
+    @FXML private Button btn_next;
+
+    @FXML private TableView<ModelGroupMember> table_memgroup;
+    @FXML private TableColumn<ModelGroupMember, String> tbcol_memfname;
+    @FXML private TableColumn<ModelGroupMember, String> tbcol_memlname;
+    @FXML private TableColumn<ModelGroupMember, String> tbcol_memidtype;
+    @FXML private TableColumn<ModelGroupMember, String> tbcol_memidno;
+    @FXML private TableColumn<ModelGroupMember, String> tbcol_memroomno;
+
+    @FXML private TableView<ModelRoom> table_room;
+    @FXML private TableColumn<ModelRoom, String> tbcol_roomcat;
+    @FXML private TableColumn<ModelRoom, String> tbcol_roomtype;
+    @FXML private TableColumn<ModelRoom, String> tbcol_roomno;
+    @FXML private TableColumn<ModelRoom, String> tbcol_roomci;
+    @FXML private TableColumn<ModelRoom, String> tbcol_roomco;
+    @FXML private TableColumn<ModelRoom, String> tbcol_xtrabed;
+    @FXML private TableColumn<ModelRoom, String> tbcol_rprice;
+
+    @FXML private TableView<ModelFacility> table_fac;
+    @FXML private TableColumn<ModelFacility, String> tbcol_fac;
+    @FXML private TableColumn<ModelFacility, String> tbcol_facno;
+    @FXML private TableColumn<ModelFacility, String> tbcol_facprice;
+    @FXML private TableColumn<ModelFacility, String> tbcol_facbookdate;
+    @FXML private TableColumn<ModelFacility, String> tbcol_faccomment;
+
+    @FXML private TableView<ModelResv> table_sresult;
+    @FXML private TableColumn<ModelResv, String> tbcol_sresv;
+    @FXML private TableColumn<ModelResv, String> tbcol_sfname;
+    @FXML private TableColumn<ModelResv, String> tbcol_slname;
+    @FXML private TableColumn<ModelResv, String> tbcol_sroomno;
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         validation();
+    }
 
-        btn_search.setOnMouseClicked(me -> {
-            System.out.println("fired");
-            try {
-                String sql = "select * from Reservation rsv " +
-                        "Inner join Customer cust on rsv.custid = cust.custid " +
-                        "Inner join CustAddress address on cust.custid = address.custid " +
-                        "Inner join CheckInOut cio on address.custid = cio.custid " +
-                        "Inner join roombooking rbk on rsv.resvno = rbk.resvno  " +
-                        "where rsv.ResvNo like '%" + tf_search.getText() + "%'";
-
-                ObservableList<ModelRoom> rtable = FXCollections.observableArrayList();
-                ObservableList<ModelFacility> ftable = FXCollections.observableArrayList();
-                ObservableList<ModelGroupMember> gtable = FXCollections.observableArrayList();
-                ObservableList<ModelResv> resvtable = FXCollections.observableArrayList();
-
-                ResultSet data = db.executeQuery(sql);
-                while (data.next()) {
-                    ModelResv rv = new ModelResv();
-                    rv.setResvNo(data.getString("resvno"));
-                    rv.setfName(data.getString("custfname"));
-                    rv.setlName(data.getString("custlname"));
-                    rv.setRoomNo(data.getString("roomno"));
-                    resvtable.add(rv);
-                }
-
-                tbcol_sresv.setCellValueFactory(new PropertyValueFactory<>("resvNo"));
-                tbcol_sfname.setCellValueFactory(new PropertyValueFactory<>("fName"));
-                tbcol_slname.setCellValueFactory(new PropertyValueFactory<>("lName"));
-                tbcol_sroomno.setCellValueFactory(new PropertyValueFactory<>("roomNo"));
-                table_sresult.setItems(resvtable);
-
-
-            } catch (SQLException e) {
-                e.printStackTrace();
+    public void searchResv(){
+        //language=SQLite
+        ResvSearchGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (rb_resvno.isSelected()){
+                String query = "SELECT * FROM Reservation";
             }
-        });
-
-        table_sresult.setRowFactory(tv -> {
-            TableRow<ModelResv> selRow = new TableRow<>();
-            selRow.setOnMouseClicked(event -> {
-                if (event.getClickCount() == 2 && (!selRow.isEmpty())) {
-                    ModelResv rv = new ModelResv();
-                    rv = table_sresult.getSelectionModel().getSelectedItem();
-
-                    tf_resvno.clear();
-                    tf_fname.clear();
-                    tf_lname.clear();
-                    tf_address.clear();
-                    tf_city.clear();
-                    tf_idno.clear();
-                    tf_idtype.clear();
-                    tf_postcode.clear();
-                } else {
-                    try {
-                        String sql2room = "select * from Reservation rsv " +
-                                "Inner join Customer cust on rsv.custid = cust.custid " +
-                                "Inner join CustAddress address on cust.custid = address.custid " +
-                                "Inner join CheckInOut cio on address.custid = cio.custid " +
-                                "Inner join roombooking rbk on rsv.resvno = rbk.resvno  " +
-                                "where rsv.resvno like '%" + tf_search.getText() + "%'";
-                        String sql3facility = "select * from FacBookedDate fbd " +
-                                "INNER JOIN FacType ftype on fbd.FacNo = ftype.FacNo " +
-                                "where ResvNo =" + tf_search.getText();
-                        String sql4groupmember = " select * from CustomerGroup custgroup " +
-                                "inner join customer cust on custgroup.G_CustID = cust.CustID " +
-                                "INNER JOIN Reservation rsv on cust.CustID = rsv.CustID " +
-                                "where rsv.ResvNo = " + tf_search.getText();
-                        ObservableList<ModelRoom> rtable = FXCollections.observableArrayList();
-                        ObservableList<ModelFacility> ftable = FXCollections.observableArrayList();
-                        ObservableList<ModelGroupMember> gtable = FXCollections.observableArrayList();
-
-                        ResultSet data2 = SlideMenuController.db.executeQuery(sql2room);
-                        String resvnumber = data2.getString("resvno");
-                        String firstname = data2.getString("CustFName");
-                        String lastname = data2.getString("CustLName");
-                        String address = data2.getString("Address");
-                        String postcode = data2.getString("PostCode");
-                        String city = data2.getString("City");
-                        String country = data2.getString("Country");
-                        String idtype = data2.getString("CustID_Type");
-                        String idno = data2.getString("CustID");
-
-                        tf_resvno.setText(resvnumber);
-                        tf_fname.setText(firstname);
-                        tf_lname.setText(lastname);
-                        tf_address.setText(address);
-                        tf_city.setText(city);
-                        tf_idno.setText(idno);
-                        tf_idtype.setText(idtype);
-                        tf_postcode.setText(postcode);
-                        while (data2.next()) {
-                            ModelRoom rm = new ModelRoom();
-                            rm.setRoomno(data2.getString("roomno"));
-                            rm.setRtype(data2.getString("roomtypename"));
-                            rm.setCidate(data2.getString("date_ci"));
-                            rm.setCodate(data2.getString("date_co"));
-                            rtable.add(rm);
-                            roomCat.setCellValueFactory(new PropertyValueFactory<>("roomno"));
-                            roomType.setCellValueFactory(new PropertyValueFactory<>("rtype"));
-                            bookDate.setCellValueFactory(new PropertyValueFactory<>("cidate"));
-                            endDate.setCellValueFactory(new PropertyValueFactory<>("codate"));
-                            roomTable.setItems(rtable);
-                        }
-                        ResultSet data3 = SlideMenuController.db.executeQuery(sql3facility);
-                        while (data3.next()) {
-                            ModelFacility fc = new ModelFacility();
-                            fc.setBookedfac(data3.getString("facno"));
-                            fc.setFacprice(data3.getString("facprice"));
-                            fc.setBookedfacdate(data3.getString("bookdate"));
-                            fc.setBookedfactime(data3.getString("facdesc"));
-                            ftable.add(fc);
-                            bookfac.setCellValueFactory(new PropertyValueFactory<>("bookedfac"));
-                            facilityprice.setCellValueFactory(new PropertyValueFactory<>("facprice"));
-                            bookdate.setCellValueFactory(new PropertyValueFactory<>("bookedfacdate"));
-                            booktime.setCellValueFactory(new PropertyValueFactory<>("bookedfactime"));
-                            facilityTable.setItems(ftable);
-                        }
-                        ResultSet data4 = SlideMenuController.db.executeQuery(sql4groupmember);
-                        while (data4.next()) {
-                            ModelGroupMember gm = new ModelGroupMember();
-                            gm.setMemFName(data3.getString("custfname"));
-                            gm.setMemLName(data3.getString("custlname"));
-                            gm.seticNum(data3.getString("passportno"));
-                            gm.setRoomNo(data3.getString("blacklisted"));
-                            gtable.add(gm);
-                            fname.setCellValueFactory(new PropertyValueFactory<>("memFName"));
-                            lname.setCellValueFactory(new PropertyValueFactory<>("memLName"));
-                            passporticno.setCellValueFactory(new PropertyValueFactory<>("icNum"));
-                            roomno.setCellValueFactory(new PropertyValueFactory<>("roomNo"));
-                            guestTable.setItems(gtable);
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            return selRow;
         });
     }
 
@@ -245,7 +97,7 @@ public class ResvEditController implements Initializable {
         tf_address.addEventFilter(KeyEvent.KEY_TYPED, Validation.validCharNoCommaDot(50));
         tf_postcode.addEventFilter(KeyEvent.KEY_TYPED, Validation.validNo(12));
         tf_city.addEventFilter(KeyEvent.KEY_TYPED, Validation.validChar(25));
-        tf_idtype.addEventFilter(KeyEvent.KEY_TYPED, Validation.validChar(10));
+        tf_state.addEventFilter(KeyEvent.KEY_TYPED, Validation.validChar(25));
         tf_idno.addEventFilter(KeyEvent.KEY_TYPED, Validation.validNo(20));
     }
 
