8f6b5177036a3ec00a476e4c76143065857faea2
weichung034918 <weichung034918@gmail.com>
2016-04-17 13:11:51 +0800

    commit

--- src/application/assets/reports/DailyReportController.java
+++ src/application/assets/reports/DailyReportController.java
@@ -5,20 +5,20 @@ import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
-import javax.swing.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 public class DailyReportController {
-    Jframe test = new JFrame();
 
     Connection conn = DriverManager.getConnection("jdbc:sqlite:Data.sqlite");
             JasperDesign jasperdesign  = JRXmlLoader.load("daily_room.jrxml");
             JasperReport jasperreport = JasperCompileManager.compileReport(jasperdesign);
             JasperPrint jasperprint = JasperFillManager.fillReport(jasperreport,null,conn);
     JasperViewer.viewReport();
+    //test
     public DailyReportController() throws JRException, SQLException {
     }
 }
