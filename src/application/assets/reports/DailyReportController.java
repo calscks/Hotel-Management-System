e994d5af2054c1859bf73cb7886602400907b4ac
weichung034918 <weichung034918@gmail.com>
2016-04-17 13:09:57 +0800

    reports and its jar files

--- src/application/assets/reports/DailyReportController.java
+++ src/application/assets/reports/DailyReportController.java
@@ -1,7 +1,24 @@
 package application.assets.reports;
-//testestest
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class DailyReportController {
+    Jframe test = new JFrame();
 
+    Connection conn = DriverManager.getConnection("jdbc:sqlite:Data.sqlite");
+            JasperDesign jasperdesign  = JRXmlLoader.load("daily_room.jrxml");
+            JasperReport jasperreport = JasperCompileManager.compileReport(jasperdesign);
+            JasperPrint jasperprint = JasperFillManager.fillReport(jasperreport,null,conn);
+    JasperViewer.viewReport();
+    public DailyReportController() throws JRException, SQLException {
+    }
 }
