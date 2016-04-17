da31f2e41c34b7cf12400be6d5a8b2b49fdc0f0f
lolzcalvin <brotherken1996@gmail.com>
2016-04-17 13:55:46 +0900

    now go and fix

--- src/application/assets/reports/DailyReportController.java
+++ src/application/assets/reports/DailyReportController.java
@@ -1,24 +1,39 @@
 package application.assets.reports;
 
+import javafx.fxml.Initializable;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 
-public class DailyReportController {
+public class DailyReportController  implements Initializable{
 
-    Connection conn = DriverManager.getConnection("jdbc:sqlite:Data.sqlite");
-            JasperDesign jasperdesign  = JRXmlLoader.load("daily_room.jrxml");
-            JasperReport jasperreport = JasperCompileManager.compileReport(jasperdesign);
-            JasperPrint jasperprint = JasperFillManager.fillReport(jasperreport,null,conn);
-    JasperViewer.viewReport();
-    //test
-    public DailyReportController() throws JRException, SQLException {
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:Data.sqlite");
+            try {
+                JasperDesign jd = JRXmlLoader.load(getClass().getResourceAsStream("/application/assets/reports/daily_room.jrxml"));
+                JasperReport jasperreport = JasperCompileManager.compileReport(jd);
+                JasperPrint jasperprint = JasperFillManager.fillReport(jasperreport,null,conn);
+                JasperViewer.viewReport(jasperprint, false);
+            } catch (JRException e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
